82f35fbe1b0e866b9cd9220afcde3616dd945b6f
João Bruno <j_bruno_s@hotmail.com>
2020-05-02 16:29:31 -0300

    altera o tipo de dado das migrations

--- src/database/migrations/20200502161637_create_disponibilidade.js
+++ src/database/migrations/20200502161637_create_disponibilidade.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('disponibilidade', function(table){
         table.increments().primary()
         table.integer('dia')
-        table.string('professor_id')
+        table.integer('professor_id')
         table.integer('escola_id')
         
         table.foreign('escola_id').references('escola.id')
