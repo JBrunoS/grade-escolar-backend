93d56bdf642e42d33a8f503041b4b0fbb37b6e3f
João Bruno <j_bruno_s@hotmail.com>
2020-05-16 09:59:36 -0300

    altera tipo de dado salvo no banco

--- src/database/migrations/20200516064803_create_observacoes.js
+++ src/database/migrations/20200516064803_create_observacoes.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('observacao', function(table){
       table.increments().primary()
       table.string('descricao')
-      table.date('data')
-      table.time('hora')
+      table.string('data')
+      table.string('hora')
       table.integer('grade_id')
       table.integer('escola_id')
 
