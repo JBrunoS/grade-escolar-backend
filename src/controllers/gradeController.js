21ffc8b2fea5b39dceb7f5236adbc58ca92a3016
João Bruno <j_bruno_s@hotmail.com>
2020-05-25 14:26:21 -0300

    excluindo dados da tabela observação

--- src/controllers/gradeController.js
+++ src/controllers/gradeController.js
@@ -168,7 +168,7 @@ module.exports = {
             return response.status(401).json({ error: 'Esse usuário não está cadastrado na banco de dados' })
         }
         
-        await connection('observacao').where('turma_id', id).delete();
+        await connection('observacao').where('grade_id', id).delete();
         await connection('grade').where('id', id).delete();
 
         return response.json(data)
